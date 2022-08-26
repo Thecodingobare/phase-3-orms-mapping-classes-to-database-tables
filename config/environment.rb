9dc507b11b54953fbe4a54383042bb07ad2aa74e
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-26 11:27:38 +0300

    completed

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,9 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+
+
+#Creating table
+Song.create_table
+
+song = Song.create(name: "Hello", album: "25")
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,46 @@
 class Song
 
-  attr_accessor :name, :album
+    attr_accessor :name, :album, :id
+  
+    def initialize(name:, album:,id:nil)
+      @name = name
+      @album = album
+    end
 
-  def initialize(name:, album:)
-    @name = name
-    @album = album
+    def self.create_table
+        sql=<<-SQL
+        CREATE TABLE IF NOT EXISTS songs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            album TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    #Inserting data into table we've created above
+    def save
+        sql=<<-SQL
+            INSERT INTO songs(
+                name,
+                album
+            )
+            VALUES (?,?)
+        SQL
+        DB[:conn].execute(sql, self.name,self.album)
+
+        #Retreiving id
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+        #return the ruby instance
+        self
+    end
+
+    #Creating a method of creating a new song to abstract away all the repetitive code
+
+    def self.create(name:,album:)
+        song = Song.new(name: name, album: album)
+        song.save
+    end
+
+  
   end
-
-end
